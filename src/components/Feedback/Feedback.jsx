c069a23f915af9152265b2fde560382506f6683d
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2023-02-16 19:54:19 +0100

    update

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { Title, Btn } from './Feedback.styled';
 import { Fragment } from 'react';
 
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NotyText } from './Notification.styled';
 
 export const Noticifaciton = prosp => {
